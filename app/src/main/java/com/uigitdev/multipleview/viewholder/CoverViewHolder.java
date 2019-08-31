6d46ef5e4d3fc5b99143fc0b94e682b8e45f4ee1
uigitdev <ui.gitdev@gmail.com>
2019-08-31 15:38:21 +0200

    Upload Project

---
+++ app/src/main/java/com/uigitdev/multipleview/viewholder/CoverViewHolder.java
@@ -0,0 +1,21 @@
+package com.uigitdev.multipleview.viewholder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.uigitdev.multipleview.R;
+
+public class CoverViewHolder extends RecyclerView.ViewHolder {
+    public ImageView item_cover_picture;
+    public RelativeLayout cover_parent;
+
+    public CoverViewHolder(@NonNull View itemView) {
+        super(itemView);
+        item_cover_picture = itemView.findViewById(R.id.item_cover_picture);
+        cover_parent = itemView.findViewById(R.id.cover_parent);
+    }
+}
